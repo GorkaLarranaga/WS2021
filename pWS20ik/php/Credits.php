c072139fa001762faeceee778bf09eb288bd525b
GorkaLarranaga <76711201+GorkaLarranaga@users.noreply.github.com>
2020-12-29 00:22:10 +0100

    Add files via upload

---
+++ pWS20ik/php/Credits.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include '../html/Head.html'?>
+
+		<style>
+			.center {
+			  margin: auto;
+			  width: 50%;
+			  padding: 10px;
+			}
+
+			table, tr, td, th{
+				text-align: left;
+				padding: 10px;
+			}
+		</style>
+
+	</head>
+	<body>
+		<?php include '../php/Menus.php' ?>
+		<section class="main" id="s1">
+			<div>
+
+				<h2>EGILEAREN/EN DATUAK</h2>
+				<div class="center">
+					<table>
+						<tr>
+							<th>Deiturak: </th>
+							<td>Gorka Larrañaga</td>
+						</tr>
+						<tr>
+							<th>Espezialitatea: </th>
+							<td>Software Ingeniaritza</td>
+						</tr>
+						<tr>
+							<th>Argazkia: </th>
+							<td/>
+						</tr>
+						<tr>
+							<th>Udalerria: </th>
+							<td>Eibar</td>
+						</tr>
+
+					</table>
+				</div
+
+				</div>
+			</section>
+			<?php include '../html/Footer.html' ?>
+		</body>
+	</html>
+	
